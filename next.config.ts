f5d580cbfacdd85abda3caae3cb24be39f9359ca
MiroYar <30885391+MiroYar@users.noreply.github.com>
2025-08-05 15:58:16 +0700

    [feat] dynamic env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    TEST_ENV: process.env.TEST_ENV,
+    NEXT_PUBLIC_TEST_ENV: process.env.NEXT_PUBLIC_TEST_ENV,
+  },
 };
 
 export default nextConfig;
